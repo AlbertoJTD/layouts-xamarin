eafc78ea3cf3313c3c8fe9d57efdf602dd56f7b7
AlbertoJTD <albertojtd@outlook.com>
2023-07-02 22:21:56 -0600

    Add relative layout

--- LayoutsXamarin/LayoutsXamarin/App.xaml.cs
+++ LayoutsXamarin/LayoutsXamarin/App.xaml.cs
@@ -10,7 +10,7 @@ namespace LayoutsXamarin
 		{
 			InitializeComponent();
 
-			MainPage = new AbsoluteLayoutDemo();
+			MainPage = new RelativeLayoutDemo();
 		}
 
 		protected override void OnStart()
